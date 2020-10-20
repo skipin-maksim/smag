f2c260f97d68417b3a5a8b2dfe4027ebd2d6e87d
Maksim Skipin <skipysmaks@gmail.com>
2020-10-20 22:49:30 +0300

    Updates

---
+++ precache-manifest.2f6b223a1fa0ec96f72348b9584cce8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08332dd7eb0157788ede7a2c3cb21e89",
+    "url": "/smag/index.html"
+  },
+  {
+    "revision": "8c7583c659f02c99ffe0",
+    "url": "/smag/static/css/2.3d72dcb8.chunk.css"
+  },
+  {
+    "revision": "171d34a1e84930618b59",
+    "url": "/smag/static/css/main.7a7cabe3.chunk.css"
+  },
+  {
+    "revision": "8c7583c659f02c99ffe0",
+    "url": "/smag/static/js/2.2631a129.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/smag/static/js/2.2631a129.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "171d34a1e84930618b59",
+    "url": "/smag/static/js/main.2fd0625f.chunk.js"
+  },
+  {
+    "revision": "4780e95bdde572bd82fa",
+    "url": "/smag/static/js/runtime-main.c0069826.js"
+  }
+]);
