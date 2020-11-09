a3dfbbf25f3d2887c7b53af733486cc5a80b1ddc
Aleksey Grygorenko <aleksey2892@gmail.com>
2020-11-09 21:46:06 +0300

    fix url privat bank api

--- src/services/apiPrivatBank.js
+++ src/services/apiPrivatBank.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const changeBaseUrl = () =>
-  (axios.defaults.baseURL = 'https://api.rivatbank.ua/');
+  (axios.defaults.baseURL = 'https://api.privatbank.ua/');
 
 const pickUpCurrencyData = async () => {
   changeBaseUrl();
