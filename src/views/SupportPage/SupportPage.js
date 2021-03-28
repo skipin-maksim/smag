b6a1668a37c39c870e99a65c81ffc196bd353f4a
Maksim Skipin <skipysmaks@gmail.com>
2021-03-28 15:50:52 +0300

    added info for this app

--- src/views/SupportPage/SupportPage.js
+++ src/views/SupportPage/SupportPage.js
@@ -1,5 +1,32 @@
 import React from 'react';
 
+import s from './SupportPage.module.scss';
+
 export default function SupportPage() {
-  return <div className="testViews">SupportPage</div>;
+  return (
+    <>
+      <h2 className="testViews">Info Page</h2>
+      <div className={s.container}>
+        <h3 className={s.title}>Реализовано:</h3>
+        <ul>
+          <li>
+            Создание, удаление, редактирование заказов. В заказе можно
+            использовать артикулы: 00082, 01042, 0101, 0101-1 (на самом деле их
+            около 750)
+          </li>
+          <li>Создание, удаление, клиентов</li>
+          <li>Вся информация записывается в базу данных MongoDB</li>
+        </ul>
+
+        <h3 className={s.title}>Использовано:</h3>
+        <ul>
+          <li>React</li>
+          <li>Redux (toolkit)</li>
+          <li>Пару компонентов из Material Design</li>
+          <li>Самописный сервер NodeJs, Express, Mongoose </li>
+        </ul>
+        <h3 className={s.title}>Приложение в разработке {`=)`}</h3>
+      </div>
+    </>
+  );
 }
